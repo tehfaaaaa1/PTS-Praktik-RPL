89d4339e7cfa3146d21d280d387f2dc38245145e
TEHFAAAA <fateehganteng@gmail.com>
2023-10-05 13:29:57 +0700

    PTS Semester Ganjil 2023

--- app/src/main/java/com/example/pts_fateeh_f_hendharto/DashboardMenuActivity.kt
+++ app/src/main/java/com/example/pts_fateeh_f_hendharto/DashboardMenuActivity.kt
@@ -2,20 +2,35 @@ package com.example.pts_fateeh_f_hendharto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pts_fateeh_f_hendharto.adapter.DataItemAdapter
+import com.example.pts_fateeh_f_hendharto.databinding.ActivityDashboardMenuBinding
 import com.example.pts_fateeh_f_hendharto.databinding.ItemListItemBinding
 import com.example.pts_fateeh_f_hendharto.model.DataItem
 
 class DashboardMenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ItemListItemBinding = ItemListItemBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        var rvPopularProduct: RecyclerView = findViewById(R.id.rvPopularProduct)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard_menu)
 
+//      Tampilkan notif ketika pencet notif
+        val notif = binding.notif
+        val cvNotif = binding.cvNotif
+        notif.setOnClickListener {
+            if (cvNotif.visibility == View.GONE) {
+                cvNotif.visibility = View.VISIBLE
+            }
+            else {
+                cvNotif.visibility == View.GONE
+            }
+        }
+//        DATA BINDING UNTUK RECYCLER VIEW
+//        ENTAH MENGAPA SAYA MELAKUKAN INI
         val listItem = arrayListOf<DataItem>(
             DataItem("Macbook Pro 13 M2 Chip", R.drawable.pic1, "15", "4.6"),
             DataItem("PS5 Pro Max Ultimate", R.drawable.pic2, "10", "4.2"),
@@ -23,10 +38,14 @@ class DashboardMenuActivity : AppCompatActivity() {
         )
 
         val adapter = DataItemAdapter(listItem)
-        val linearHorizontal = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
+        val recyclerView = binding.rvPopularProduct
 
-        rvPopularProduct.adapter = adapter
-        rvPopularProduct.layoutManager = linearHorizontal
+        recyclerView.apply {
+            this.adapter = adapter
+            this.layoutManager = LinearLayoutManager(
+                this@DashboardMenuActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+
 
     }
 }
