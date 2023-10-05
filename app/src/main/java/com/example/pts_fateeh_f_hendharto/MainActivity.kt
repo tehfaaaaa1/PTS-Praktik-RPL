6b99e7d59851cca0d8a64638eac9f75535ca192c
TEHFAAAA <fateehganteng@gmail.com>
2023-10-05 08:14:14 +0700

    PTS Semester Ganjil 2023

---
+++ app/src/main/java/com/example/pts_fateeh_f_hendharto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pts_fateeh_f_hendharto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
